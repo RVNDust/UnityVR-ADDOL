d139156f3c688cc81d3258cfd4636bc0fbab898a
ExiaA5-RARV <yvan.denais@viacesi.fr>
2019-01-07 17:27:38 +0100

    add player controller and wheelbeahviour script

---
+++ Assets/ADDOL/Scripts/PlayerControllerVR.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerVR : MonoBehaviour
+{
+
+    public delegate void OnGrabPressed(GameObject Controller);
+    public static event OnGrabPressed onGrabPressed;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (onGrabPressed != null)
+        {
+            onGrabPressed(gameObject);
+        }
+    }
+}
