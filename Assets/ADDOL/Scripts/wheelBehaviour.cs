5769cf2550fbac847e3617b1fd8b0169ecce2d82
ExiaA5-RARV <yvan.denais@viacesi.fr>
2019-01-08 12:17:58 +0100

    add event grab and circular drive on wheel

--- Assets/ADDOL/Scripts/wheelBehaviour.cs
+++ Assets/ADDOL/Scripts/wheelBehaviour.cs
@@ -2,19 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelBehaviour : MonoBehaviour {
+public class wheelBehaviour : MonoBehaviour
+{
+    PlayerControllerVR ci;
+    GameObject holdingController = null;
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerControllerVR>())
+        {
+            ci = other.GetComponent<PlayerControllerVR>();
+            ci.onGrabPressed += PlayerControllerVR_onGrabPressed;
+            ci.onGrabReleased += PlayerControllerVR_onGrabReleased;
+        }
+    }
 
+    private void PlayerControllerVR_onGrabPressed(GameObject grabbedObject, GameObject controller)
+    {
+        if(grabbedObject == gameObject && holdingController == null)
+        {
+            SpringJoint sj = gameObject.AddComponent<SpringJoint>();
+            sj.connectedBody = controller.GetComponent<Rigidbody>();
+            sj.spring = 50;
+            sj.damper = 0;
+            sj.breakForce = 20000;
+            sj.breakTorque = 20000;
+            holdingController = controller;
+            Debug.Log("grabbbb");
+        }
+    }
 
-    //PlayerControllerVR.onGrabPressed(gameObject);
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerControllerVR>())
+        {
+            ci = other.GetComponent<PlayerControllerVR>();
+            ci.onGrabPressed -= PlayerControllerVR_onGrabPressed;
+            ci.onGrabReleased -= PlayerControllerVR_onGrabReleased;
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void PlayerControllerVR_onGrabReleased(GameObject grabbedObject, GameObject controller)
+    {
+        if (grabbedObject == gameObject && controller == holdingController)
+        {
+            SpringJoint sj = gameObject.GetComponent<SpringJoint>();
+            if(sj != null)
+            {
+                sj.connectedBody = null;
+                Destroy(sj);
+                Debug.Log("ungrabbbb");
+            }
+        }
+    }
 }
