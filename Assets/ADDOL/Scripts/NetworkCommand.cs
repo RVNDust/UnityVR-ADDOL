b1e90f4025f585af7af361b4f2f0a3d485a700d9
Unknown <yvan.denais@viacesi.fr>
2019-01-09 00:10:29 +0100

    Auto stash before revert of "Adding particle effects and working FPSController"

---
+++ Assets/ADDOL/Scripts/NetworkCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkCommand : NetworkBehaviour
+{
+
+    [SerializeField] GameObject FirePrefab;
+    [SerializeField] Transform fireSpawner;
+
+    [Command]
+    public void CmdStartFire()
+    {
+        GameObject fire = null;
+        // Instantiate the Snowball from the Snowball Prefab at the position of the Spawner
+        fire = GameObject.Instantiate(FirePrefab);
+
+        // Spawn the bullet to update in all client. Use the NetworkServer class to do it
+        fire.transform.position = fireSpawner.transform.position;
+
+        NetworkServer.Spawn(fire);
+    }
+}
